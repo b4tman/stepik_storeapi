9ee3c6a9630784f2676658ef6f8c2fb10e78fe30
Dmitry <b4tm4n@mail.ru>
2024-03-13 15:40:25 +0300

    tests fixtures: scope=sessoin

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -12,96 +12,96 @@ def test_app(tmpdir_factory) -> FastAPI:
     return create_app()
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def client(test_app) -> TestClient:
     return TestClient(test_app)
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def client_items(test_app) -> TestClient:
-    return TestClient(test_app, base_url="http://localhost/items")
+    return TestClient(test_app, base_url="http://testserver/items")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def client_cart(test_app) -> TestClient:
-    return TestClient(test_app, base_url="http://localhost/cart")
+    return TestClient(test_app, base_url="http://testserver/cart")
 
 
-@pytest.fixture
-def credentials_admin() -> TestClient:
+@pytest.fixture(scope="session")
+def credentials_admin() -> dict[str, str]:
     return {
         "email": "admin@example.com",
         "password": "god",
     }
 
 
-@pytest.fixture
-def credentials_manager() -> TestClient:
+@pytest.fixture(scope="session")
+def credentials_manager() -> dict[str, str]:
     return {
         "email": "ivan@example.com",
         "password": "test",
     }
 
 
-@pytest.fixture
-def credentials_user() -> TestClient:
+@pytest.fixture(scope="session")
+def credentials_user() -> dict[str, str]:
     return {
         "email": "vasya@example.com",
         "password": "",
     }
 
 
-@pytest.fixture
-def credentials_invalid_user() -> TestClient:
+@pytest.fixture(scope="session")
+def credentials_invalid_user() -> dict[str, str]:
     return {
         "email": "test",
         "password": "",
     }
 
 
-@pytest.fixture
-def credentials_not_found_user() -> TestClient:
+@pytest.fixture(scope="session")
+def credentials_not_found_user() -> dict[str, str]:
     return {
         "email": "not_found@example.com",
         "password": "",
     }
 
 
-@pytest.fixture
-def credentials_admin_invalid_password() -> TestClient:
+@pytest.fixture(scope="session")
+def credentials_admin_invalid_password() -> dict[str, str]:
     return {
         "email": "admin@example.com",
         "password": "123",
     }
 
 
-@pytest.fixture
-def item_test() -> TestClient:
+@pytest.fixture(scope="session")
+def item_test() -> dict[str, str | float]:
     return {
         "name": "ложка",
         "price": 1.2,
     }
 
 
-@pytest.fixture
-def item_invalid() -> TestClient:
+@pytest.fixture(scope="session")
+def item_invalid() -> dict[str, str]:
     return {
         "price": "test",
     }
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def email_test() -> str:
     return "test@example.com"
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def email_invalid() -> str:
     return "hello!"
 
 
-@pytest.fixture
-def created_item(client_items, credentials_admin, item_test):
+@pytest.fixture(scope="session")
+def created_item(client_items, credentials_admin, item_test) -> dict[str, str | float]:
     params = {}
     params.update(credentials_admin)
     params.update(item_test)
